6a1a28ffa89152108dd07ba9eeb1332842349f4f
ShafiaChy <shafiachy04@gmail.com>
2021-08-21 20:53:10 +0600

    second commit

--- js/macBook.js
+++ js/macBook.js
@@ -1,134 +1,115 @@
 //Calculating extra cost with the help of a function
 function extraCost(input,item){
-    const Cost = document.getElementById(item+'-cost');
-   
-   
+    const cost = document.getElementById(item+'-cost');
+    //using if-else conditions the costs are set in the innerText
     if(item == 'memory'){
+
         if(input == true){
-           
-            Cost.innerText = 0;
-       
+            cost.innerText = 0;
         }
         else{
-            
-            Cost.innerText = 160;
-         
+            cost.innerText = 180;
         }
         
     }
+
     if(item == 'storage'){
         if(input == 1){
-            Cost.innerText = 0;
-      
-            
+            cost.innerText = 0;  
         }
         else if(input == 2){
-            Cost.innerText = 100;
-          
-        
-            
+            cost.innerText = 100;  
         }
         else{
-            
-            Cost.innerText = 180;
-     
-          
+            cost.innerText = 180;
         }
         
     }
+
     if(item == 'delivery'){
+
         if(input == true){
-         
-            Cost.innerText = 0;
-       
-     
-            
+            cost.innerText = 0;
         }
         else{
-           
-            Cost.innerText = 20;
-          
-           
+            cost.innerText = 20;
         }
      
     }
-    // const total = memoryCost+storageCost+deliveryCost;
+
     totalPrice();
 
-   
 }
 
 
-//calculating total price
+//calculating total price with the help of a function
 function totalPrice(){
+    
     const memoryPrice = document.getElementById('memory-cost').innerText;
     const storagePrice = document.getElementById('storage-cost').innerText;
     const deliveryPrice = document.getElementById('delivery-cost').innerText;
-    const totalPrice1 = document.getElementById('total-price');
-    const totalPrice2 = document.getElementById('discount');
+
+    const bestPrice = document.getElementById('best-price').innerText;
+    const totalPrice = document.getElementById('total-price');
+    const total = document.getElementById('total-after-promo');
     
-    const total = 1299 + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
-    totalPrice1.innerText = total;
-    totalPrice2.innerText = total;
-    
+    const sum = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+    totalPrice.innerText = sum;
+    total.innerText = sum;
 
 }
 
+//calculating total after applying the promo code with the help of a function
 function promoCode(input){
-    const totalPriceBeforeDiscount = document.getElementById('discount').innerText;
+
+    const totalPriceBeforeDiscount = document.getElementById('total-after-promo').innerText;
+
     if(input == 'stevekaku'){
+
         const totalPriceAfterDiscount = totalPriceBeforeDiscount - ((20/100) * totalPriceBeforeDiscount);
-        document.getElementById('discount').innerText = totalPriceAfterDiscount;
+        document.getElementById('total-after-promo').innerText = totalPriceAfterDiscount;
         
     }
 }
 
-
+//sending the parameter to a function when a button is clicked
 // Extra Memory Cost
 document.getElementById('memory-8gb-btn').addEventListener('click', function (){
    extraCost(true,'memory');
- 
-   
 })
 document.getElementById('memory-16gb-btn').addEventListener('click', function (){
   extraCost(false,'memory');
-
-    
-   
 })
+
 
 // Extra Storage Cost
 document.getElementById('storage-256gb-btn').addEventListener('click', function (){
     extraCost(1,'storage');
 
 })
-
 document.getElementById('storage-512gb-btn').addEventListener('click', function (){
-    extraCost(2,'storage');
-  
+    extraCost(2,'storage'); 
+})
+document.getElementById('storage-1tb-btn').addEventListener('click', function (){
+    extraCost(3,'storage');  
 })
 
-document.getElementById('storage-1tb-btn').addEventListener('click', function (){
-    extraCost(3,'storage');
-    
-})
 
 // Delivery Cost
 document.getElementById('free-delivery').addEventListener('click', function (){
     extraCost(true,'delivery');
- 
 })
-
 document.getElementById('delivery-charge-20').addEventListener('click', function (){
-    extraCost(false,'delivery');
-     
+    extraCost(false,'delivery');    
 })
 
+
+//getting the promo code from the input field and sending it to a function
 document.getElementById('apply-btn').addEventListener('click',function (){
     const inputCode = document.getElementById('promo-code');
     const code = inputCode.value;
     promoCode(code);
-    console.log(code);
+    inputCode.value ='';
     
 })
 
